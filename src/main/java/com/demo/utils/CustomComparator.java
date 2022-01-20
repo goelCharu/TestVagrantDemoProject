68f66ab1fd50e92d0695defe8fdb9c803f35b5f2
goelCharu <98052162+goelCharu@users.noreply.github.com>
2022-01-20 14:16:46 +0530

    Adding comparator to check two different value

---
+++ src/main/java/com/demo/utils/CustomComparator.java
@@ -0,0 +1,25 @@
+package com.demo.utils;
+
+/**
+ * this class contains custom comparison for tests
+ */
+public class CustomComparator {
+
+    /**
+     *
+     * @param value1
+     * @param value2
+     * @param range range to be specified, if difference equal or smaller than range
+     *              returns true else false
+     * @return boolean value
+     */
+    public boolean comparePermissibleRange(Double value1, Double value2, Double range){
+        if (value1 == value2)
+            return value1 == value2;
+        if (value1>value2)
+            return (value1 - value2) <= range;
+        else
+            return (value2 - value1) <= range;
+    }
+
+}
