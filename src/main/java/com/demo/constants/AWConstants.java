4dc7e01cb51967fa40673a3441d22b91a88e32ee
goelCharu <98052162+goelCharu@users.noreply.github.com>
2022-01-20 14:15:05 +0530

    Adding constant file

---
+++ src/main/java/com/demo/constants/AWConstants.java
@@ -0,0 +1,13 @@
+package com.demo.constants;
+
+public interface AWConstants {
+    /**
+     * Constants that can be used for Accu Weather site
+     */
+     String API_KEY = "7fe67bf08c80ded756e598d6f8fedaea";
+     String BASE_URL = "https://api.openweathermap.org/";
+     String BASE_PATH = "data/2.5/weather";
+     String URL = "https://www.accuweather.com/";
+     String LOCATION = "Noida, Uttar pradesh";
+     double KELVIN = 273.15;
+}
